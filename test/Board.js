a31c0522b927fb74c24bab10fa25924db8311d64
kosmobot <jeppe@burchardt.com>
2014-09-16 00:06:19 +0200

    add more failing tests

--- test/Board.js
+++ test/Board.js
@@ -4,6 +4,15 @@ var Board = require('../src/Board');
 
 var board = null;
 
+
+function isMoveInPermutations (moves, permutations) {
+	return permutations.some(function (p) {
+		// console.log('P:', p.moves, 'M:', moves);
+		return JSON.stringify(p.moves) === JSON.stringify(moves);
+	});
+}
+
+
 beforeEach(function(){
 
 	board = new Board();
@@ -78,9 +87,9 @@ describe('Board', function () {
 			assert.notEqual(false, board.isMoveIllegal(0, 1, 1));
 		});
 
-		it('cannot move a checker to a tile occupied by 5 checkers of own kind', function () {
+		it('can move a checker to a tile occupied by 5 checkers of own kind', function () {
 			board.players[0].checkers[6] = 1;
-			assert.notEqual(false, board.isMoveIllegal(0, 6, 5));
+			assert.equal(false, board.isMoveIllegal(0, 6, 5));
 		});
 
 		it('can not bear off checker if hit', function () {
@@ -94,13 +103,71 @@ describe('Board', function () {
 			assert.notEqual(false, board.isMoveIllegal(0, 22, 2));
 		});
 
+	});
+
+
+	describe('#getAllPermutations', function () {
+
 		it('can not bear off a checker on a die roll, that is higher than required, if there is another checker to move', function () {
 			board.players[0].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1];
-			assert.notEqual(false, board.isMoveIllegal(0, 23, 2));
+			board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1];
+			var p = board.getAllPermutations(0, [2, 2]);
+			assert.equal(isMoveInPermutations([[23, 2], [21, 2]], p), false);
 		});
 
+		it('should bear off before making another move', function () {
+			board.players[0].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0];
+			board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0];
+			var p = board.getAllPermutations(0, [2, 1]);
+			assert.equal(isMoveInPermutations([[21, 2], [22, 1]], p), false);
+		});
 
+		it('should only allow the highest die, if only one or the other can be used', function () {
+			board.players[0].checkers = [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0];
+			var p = board.getAllPermutations(0, [2, 3]);
+			assert.equal(isMoveInPermutations([[2, 3]], p), true);
+			assert.equal(isMoveInPermutations([[2, 2], [4, 3]], p), false);
+			assert.equal(isMoveInPermutations([[2, 3], [5, 2]], p), false);
+			assert.equal(p.length, 1);
+		});
+
+		it('should allow to use any order od the dice, if there is no obstacles', function () {
+			board.players[0].checkers = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			var p = board.getAllPermutations(0, [1, 2]);
+			assert.equal(isMoveInPermutations([[0, 1], [1, 2]], p), true);
+			assert.equal(isMoveInPermutations([[0, 2], [2, 1]], p), true);
+			assert.equal(p.length, 2);
+		});
+
+		it('should find a way around an opponent', function () {
+			board.players[0].checkers = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0];
+			var p = board.getAllPermutations(0, [1, 2]);
+			assert.equal(isMoveInPermutations([[0, 1], [1, 2]], p), true);
+			assert.equal(p.length, 1);
+		});
+
+		it('should not be possible to make any moves, if all tiles are blocked by the opponent', function () {
+			board.players[0].checkers = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,0];
+			var p = board.getAllPermutations(0, [1, 2]);
+			assert.equal(p[0].moves.length, 0);
+			assert.equal(p[1].moves.length, 0);
+		});
+
+		it('should not be possible to move a hit checker, if all tiles are blocked by the opponent', function () {
+			board.players[0].checkers = [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			board.players[0].hits = 1;
+			board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2];
+			var p = board.getAllPermutations(0, [5, 4]);
+			assert.equal(p[0].moves.length, 0);
+			assert.equal(p[1].moves.length, 0);
+		});
 
 	});
+
+
 
 });
--- test/Judge.js
+++ test/Judge.js
@@ -14,23 +14,31 @@ describe('Judge', function () {
 
 	describe('#checkGameIntegrity', function () {
 
-		it('should fail if there is to many checkers', function () {
+		it('should return false if there is to many checkers', function () {
 			game.board.players[0].checkers[24] = 1;
 			assert.equal(game.judge.checkGameIntegrity(), false);
 		});
 
-		it('should fail if sum of player\'s checkers is not 15', function () {
+		it('should return false if sum of player\'s checkers is not 15', function () {
 			game.board.players[0].hits = 1;
 			assert.equal(game.judge.checkGameIntegrity(), false);
 		});
 
-		it('should fail if sum of player\'s checkers is not 15', function () {
+		it('should return false if sum of player\'s checkers is not 15', function () {
 			game.board.players[0].bearedOff = 1;
 			assert.equal(game.judge.checkGameIntegrity(), false);
 		});
 
-		it('should fail if sum of player\'s checkers is not 15', function () {
+		it('should return false if sum of player\'s checkers is not 15', function () {
 			game.board.players[0].checkers[0] = 1;
+			assert.equal(game.judge.checkGameIntegrity(), false);
+		});
+
+		it('should return false if more than one player is occupying the same tile', function () {
+			game.board.players[0].hits = 14;
+			game.board.players[1].hits = 14;
+			game.board.players[0].checkers = [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			game.board.players[1].checkers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0];
 			assert.equal(game.judge.checkGameIntegrity(), false);
 		});
 
