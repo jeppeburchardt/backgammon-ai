cc4b17d3363c79bdda44ad6521a8042b79e36912
kosmobot <jeppe@burchardt.com>
2014-10-03 15:54:51 +0200

    add controllers to export

--- src/index.js
+++ src/index.js
@@ -11,8 +11,13 @@ module.exports = {
 	controllers : {
 		Aggressive : require('./controllers/Aggressive'),
 		PrimerEndGame : require('./controllers/PrimerEndGame'),
+		PrimerEndGame2 : require('./controllers/PrimerEndGame2'),
 		Random : require('./controllers/Random'),
-		Runner : require('./controllers/Runner')
+		Runner : require('./controllers/Runner'),
+		NotBetterWithGetScore: require/('./controllers/NotBetterWithGetScore'),
+		NotBetterWith357: require/('./controllers/NotBetterWith357'),
+		TestA: require/('./controllers/TestA'),
+		TestB: require/('./controllers/TestB')
 	}
 
 }
